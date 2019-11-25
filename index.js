09e29b8ea6a87049618946c482a72465f38a3c60
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2019-11-25 15:12:25 -0700

    added more key value pairs and if statements to functions within object

--- index.js
+++ index.js
@@ -2,31 +2,56 @@
 
 
 var password = {
-    lowerCaseArr: ['abcdefghijklmnopqrstuvwxyz'],
-    upperCaseArr: ['ABCDEFGHIJKLMONPQRSTUVWXYZ'],
-    symbolsArr: ['~`!@#$%^&*()={}?<>:;'],
+    serPassword: [],
+    passwordLength: null,
+    containsSpecial: false,
+    containsUppercase: false,
+    containsLowercase: false,
+    lowerCaseArr: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    upperCaseArr: ["A","B","C","D","E","F","G","H","I","J","K","L","M","O","N","P","Q","R","S","T","U","V","W","X","Y","Z"],
+    symbolsArr: ["~","`","!","@","#","$","%","^","&","*","(",")","=","{","}","?","<",">",":",";"],
     numbersArr: [1,2,3,4,5,6,7,8,9,0],
 
     inputLength: function() {
         var lengthInput = prompt("Pick a number between a 8 and 128 for the password length");
           lengthInput = parseInt(lengthInput)
         
+        if(lengthInput >= 8 && lengthInput <= 128) {
+            password.passwordLength = lengthInput
+        } else {
+            inputLength();
+        }
+          
         console.log(`Your password will be ${lengthInput} characters long`)
     },
 
     symbolInput: function() {
         var symbols = confirm("Would you like special characters?");
+
+        if (symbols) {
+            password.containsSpecial = true
+          }
         
         console.log(symbols)
     },
 
     randomLower: function() {
         var lowerCase = confirm("Would you like lower case characters?");
+
+        if (lowerCase) {
+            password.containsLowercase = true
+          }
+
          console.log(lowerCase)
     },
 
     randomUpper: function() {
         var upperCase = confirm("Would you like upper case characters?");
+
+        if (upperCase) {
+            password.containsUppercase = true
+          }
+          
         console.log(upperCase)
     }
 }
