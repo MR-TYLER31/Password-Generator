deb03526807f8394a561069b067e877da8da2a2c
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2019-12-03 00:02:47 -0700

    adding event listener for password generation

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ var lowerCaseEl,
     numberEl,
     symbolEl,  
     lengthEl,
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol;
+    length,
+    lowercase,
+    uppercase,
+    symbol,
+    number;
 
    var masterPassword= [];
    var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -26,49 +27,61 @@ var lowerCaseEl,
 
    function inputLength() {
             var lengthEl = prompt("Pick a number between a 8 and 128 for the password length");
-            
+            var length = parseInt(lengthEl);
             if(lengthEl >= 8 && lengthEl <= 128) {
-                console.log(`Your password will be ${lengthEl} characters long`);
-                console.log(parseInt(lengthEl));
+                console.log(parseInt(length));
             } else {
                inputLength();
             }     
         }
     function userInput() {
+        // Asks user if they want to use lowercase letters
         var lowerCaseEl = confirm("Would you like lower case characters?");
+        var lowercase = lowerCaseEl;
             if(lowerCaseEl) {      
-                masterPassword += lowerCaseArr      
-                console.log(lowerCaseEl)
-
+                masterPassword += lowerCaseArr;      
+                console.log(lowercase)
             }  
 
+            // Asks user if they want to use uppercase letters
             var upperCaseEl = confirm("Would you like upper case characters?");
+            var uppercase = upperCaseEl;
             if(upperCaseEl) {   
-                masterPassword += upperCaseArr       
+                masterPassword += upperCaseArr;   
                 console.log(upperCaseEl);
             }
 
+            // Asks user if they want to use a symbol
             var symbolEl = confirm("Would you like special characters?");
+            var symbol = symbolEl;
             if(symbolEl) {    
                 masterPassword += symbolArr          
-                console.log(symbolEl)
+                console.log(symbol)
             }
 
+            // Asks user if they want to use a number
             var numberEl = confirm("Would you like numbers?");
+            var number = numberEl;
             if(numberEl) { 
                 masterPassword += numberArr                     
-                console.log(numberEl)
+                console.log(number)
             }
 
             console.log(masterPassword)
     }
 
-     // Click Event for generate password button
-     generateBtn.addEventListener('click', function() {
-       
-    });
+
+    // click even to generate random password from users input
+generateBtn.addEventListener('click', function() {
+
+    
+});
+
     
 
+ 
+// pull in selected input from user
+// take user input and randomize by the length of the user input length
 
 
 inputLength();
