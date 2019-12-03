1825b072ef98975669670b8262f05fbd0dd923aa
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2019-12-02 22:36:08 -0700

    refactored code version 2

--- index.js
+++ index.js
@@ -1,115 +1,193 @@
 // Generate a random password with the input provided by the user
+var resultEl = document.getElementById('password');
+var generateBtn = document.getElementById('generate');
+var copyBtn = document.getElementById('copy');
 
 
-var password = {
-    userPassword: [],
-    passwordLength: null,
-    containsSpecial: false,
-    containsUppercase: false,
-    containsLowercase: false,
-    containsNum: false,
-    lowerCaseArr: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-    upperCaseArr: ["A","B","C","D","E","F","G","H","I","J","K","L","M","O","N","P","Q","R","S","T","U","V","W","X","Y","Z"],
-    symbolsArr: ["~","`","!","@","#","$","%","^","&","*","(",")","=","{","}","?","<",">",":",";"],
-    numbersArr: [1,2,3,4,5,6,7,8,9,0],
+
+// Global Variables
+var lowerCaseEl,
+    upperCaseEl,
+    numberEl,
+    symbolEl,  
+    lengthEl,
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol;
+
+   var masterPassword= [];
+   var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+   var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+   var symbolArr = ['!','@','#','$','%','^','&','*','(',')','_','+','=','/','{','}'];
+   var numberArr = [0,1,2,3,4,5,6,7,8,9];
+
+ 
+
+   function inputLength() {
+            var lengthEl = prompt("Pick a number between a 8 and 128 for the password length");
+            
+            if(lengthEl >= 8 && lengthEl <= 128) {
+                console.log(`Your password will be ${lengthEl} characters long`);
+                console.log(parseInt(lengthEl));
+            } else {
+               inputLength();
+            }     
+        }
+    function userInput() {
+        var lowerCaseEl = confirm("Would you like lower case characters?");
+            if(lowerCaseEl) {      
+                masterPassword += lowerCaseArr      
+                console.log(lowerCaseEl)
+
+            }  
+
+            var upperCaseEl = confirm("Would you like upper case characters?");
+            if(upperCaseEl) {   
+                masterPassword += upperCaseArr       
+                console.log(upperCaseEl);
+            }
+
+            var symbolEl = confirm("Would you like special characters?");
+            if(symbolEl) {    
+                masterPassword += symbolArr          
+                console.log(symbolEl)
+            }
+
+            var numberEl = confirm("Would you like numbers?");
+            if(numberEl) { 
+                masterPassword += numberArr                     
+                console.log(numberEl)
+            }
+
+            console.log(masterPassword)
+    }
+
+     // Click Event for generate password button
+     generateBtn.addEventListener('click', function() {
+       
+    });
     
-    generateLowerCase: function(){
-        var letterLower = this.lowerCaseArr[Math.floor(Math.random() * 26)]
-        console.log(letterLower);
-    },
-
-    generateUpperCase: function(){
-        var letterUpper = this.upperCaseArr[Math.floor(Math.random() * 26)]
-        console.log(letterUpper);
-    },
-
-    generateSymbol: function(){
-        var randSymbol = this.symbolsArr[Math.floor(Math.random() * 20)]
-        console.log(randSymbol);
-    },
-
-    generateNum: function(){
-        var randNum = this.numbersArr[Math.floor(Math.random() * 20)]
-        console.log(randNum);
-    },
 
 
-    inputLength: function() {
-        var lengthInput = prompt("Pick a number between a 8 and 128 for the password length");
-          lengthInput = parseInt(lengthInput)
+
+inputLength();
+userInput();
+
+
+
+
+// password min length 8 and max of 128
+// 1. how to get info from user
+// 2. do something with info
+        // a. check if user wants upper case
+        // b. check if user want lower case
+        // c. check if user wants special character
+        // d. check if user wants a number
+            // if yes than 
+                // console the input
+// 2. 
         
-        if(lengthInput >= 8 && lengthInput <= 128) {
-            password.passwordLength = lengthInput
-            console.log(`Your password will be ${lengthInput} characters long`)
-        } else {
-            password.inputLength();
-        }     
-    },
 
-    symbolInput: function() {
-        var symbolsPrompt = confirm("Would you like special characters?");
 
-        if (symbolsPrompt) {
-            password.containsSpecial = true
-          }
+
+
+
+
+//     // Generate random character
+//     function generateLowerCase(){
+//         var letterLower = lowerCaseArr[Math.floor(Math.random() * 26)]
+//         console.log(letterLower);
+//     }
+
+//     // Generate random character
+//     function generateUpperCase(){
+   
+//             var letterUpper = upperCaseArr[Math.floor(Math.random() * 26)]
+//         console.log(letterUpper);     
+//    }
+
+//     // Generate random character
+//     function generateSymbol(){
+//         var randSymbol = symbolsArr[Math.floor(Math.random() * 20)]
+//         console.log(randSymbol);
+//     }
+
+//     // Generate random character
+//     function generateNum(){
+//         var randNum = numbersArr[Math.floor(Math.random() * 10)];
+//         console.log(randNum);
+//     }
+
+//     // Get the length input from user
+//     function inputLength() {
+//         var lengthEl = prompt("Pick a number between a 8 and 128 for the password length");
         
-        console.log(symbolsPrompt)
-    },
+//         if(lengthEl >= 8 && lengthEl <= 128) {
+//             console.log(`Your password will be ${lengthEl} characters long`);
+//             console.log(parseInt(lengthEl));
+//         } else {
+//            inputLength();
+//         }     
+//     }
 
-    lowerInput: function() {
-        var lowerCase = confirm("Would you like lower case characters?");
+//     // Get the symbol input from user
+//     function symbolInput() {
+//         var symbolsEl = confirm("Would you like special characters?");
 
-        if (lowerCase) {
-            password.containsLowercase = true
-          }
+//         if (symbolsEl) {
+//             console.log(symbolsEl)
+//           }      
+//     }
 
-         console.log(lowerCase)
-    },
+//     // Get the lowercase input from user
+//     function lowerInput() {
+//         var lowerCaseEl = confirm("Would you like lower case characters?");
 
-    upperInput: function() {
-        var upperCase = confirm("Would you like upper case characters?");
+//         if (lowerCaseEl) {
+//             console.log(lowerCaseEl)
+//           }   
+//     }
 
-        if (upperCase) {
-            password.containsUppercase = true
-          }
+//     // Get the uppercase input from user
+//     function upperInput() {
+//         var upperCaseEl = confirm("Would you like upper case characters?");
 
-        console.log(upperCase)
-    },
-    numInput: function() {
-        var num = confirm("Would you like numbers?");
+//         if (upperCaseEl) {
+//             console.log(upperCaseEl);
+//           }
+//     }
 
-        if (num) {
-            password.containsNum = true
-          }
+//     // Get the number input from user
+//     function numInput() {
+//         var numberEl = confirm("Would you like numbers?");
 
-        console.log(num)
-    },
-}
+//         if (numberEl) {
+//             console.log(numberEl)
+//           } 
+//         }
+
+   
 
 
-// password.inputLength()
-// password.symbolInput()
-// password.lowerInput()
-// password.upperInput()
-// password.numInput()
-// password.generateLowerCase()
-// password.generateUpperCase()
-// password.generateSymbol()
-// password.generateNum()
+  
+
+  
+
+
+// inputLength()
+// symbolInput()
+// lowerInput()
+// upperInput()
+// numInput()
+// generateLowerCase()
+// generateUpperCase()
+// generateSymbol()
+// generateNum()
+
+
 
 
 
 // alert('Click the generate button to see your new password')
 
-
-
-
-
-
-
-
-
-
-
-
-// function that takes the input from user and randomizes into a string 
