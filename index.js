9805b13a43e88b9b637cf9bf76aedf78263899e5
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2019-12-03 21:34:49 -0700

    added event listener for copy to clipboard and set timeout messages

--- index.js
+++ index.js
@@ -2,104 +2,123 @@
 var resultEl = document.getElementById('password');
 var generateBtn = document.getElementById('generate');
 var copyBtn = document.getElementById('copy');
-
+var message = document.getElementById('message')
 
 
 // Global Variables
 var lowerCaseEl,
-    upperCaseEl,
-    numberEl,
-    symbolEl,  
-    lengthEl,
-    length,
-    lowercase,
-    uppercase,
-    symbol,
-    number;
-
-   var masterPassword= [];
-   var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-   var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-   var symbolArr = ['!','@','#','$','%','^','&','*','(',')','_','+','=','/','{','}'];
-   var numberArr = [0,1,2,3,4,5,6,7,8,9];
-
- 
-
-   function inputLength() {
-       // Ask the user how long they want their password
-            var lengthEl = prompt("Pick a number between a 8 and 128 for the password length");
-            var length = parseInt(lengthEl);
-            if(lengthEl >= 8 && lengthEl <= 128) {
-                console.log(parseInt(length));
-            } else {
-               inputLength();
-            }     
-        }
-    function userInput() {
-        // Asks user if they want to use lowercase letters
-        var lowerCaseEl = confirm("Would you like lower case characters?");
-        var lowercase = lowerCaseEl;
-            if(lowerCaseEl) {      
-                masterPassword += lowerCaseArr;      
-                console.log(lowercase)
-            }  
-
-            // Asks user if they want to use uppercase letters
-            var upperCaseEl = confirm("Would you like upper case characters?");
-            var uppercase = upperCaseEl;
-            if(upperCaseEl) {   
-                masterPassword += upperCaseArr;   
-                console.log(upperCaseEl);
-            }
-
-            // Asks user if they want to use a symbol
-            var symbolEl = confirm("Would you like special characters?");
-            var symbol = symbolEl;
-            if(symbolEl) {    
-                masterPassword += symbolArr          
-                console.log(symbol)
-            }
-
-            // Asks user if they want to use a number
-            var numberEl = confirm("Would you like numbers?");
-            var number = numberEl;
-            if(numberEl) { 
-                masterPassword += numberArr                     
-                console.log(number)
-            }
-
-            console.log(masterPassword)
-    }
+upperCaseEl,
+numberEl,
+symbolEl,  
+lengthEl,
+length,
+lowercase,
+uppercase,
+symbol,
+number;
 
 
-    // click even to generate random password from users input
-generateBtn.addEventListener('click', function() {
-
-    
-});
-
-    
+var masterPassword= '';
+var lowerCaseArr = 'abcdefghkjklmnopqrstuvwxyz';
+var upperCaseArr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var symbolArr = '!@#$%^&*()_+=/{}';
+var numberArr = '0123456789';
 
  
+// Click event to fire off the user prompts and gather user input to formulate the password functions
+generateBtn.addEventListener('click', generatePassword);
+   
+// function to ask for users input and conditional logic
+function generatePassword() {
+        var lengthEl = prompt("Pick a number between a 8 and 128 for the password length");
+             var length = parseInt(lengthEl);
+             if(lengthEl >= 8 && lengthEl <= 128) {
+                 console.log(parseInt(length));
+             } else {
+                generatePassword();
+             }     
 
+         // Asks user if they want to use lowercase letters
+         var lowerCaseEl = confirm("Would you like lower case characters?");
+         var lowercase = lowerCaseEl;
+             if(lowerCaseEl) {           
+                 masterPassword += lowerCaseArr.slice(0,8);    
+                 console.log(lowercase)
+             }  
+ 
+             // Asks user if they want to use uppercase letters
+             var upperCaseEl = confirm("Would you like upper case characters?");
+             var uppercase = upperCaseEl;
+             if(upperCaseEl) {   
+                 masterPassword += upperCaseArr.slice(0,8);;   
+                 console.log(upperCaseEl);
+             }
+ 
+             // Asks user if they want to use a symbol
+             var symbolEl = confirm("Would you like special characters?");
+             var symbol = symbolEl;
+             if(symbolEl) {   
+                 masterPassword += symbolArr.slice(0,8);          
+                 console.log(symbol);
 
-inputLength();
-userInput();
+             }
+ 
+             // Asks user if they want to a number
+             var numberEl = confirm("Would you like numbers?");
+             var number = numberEl;
+             if(numberEl) { 
+                 masterPassword += numberArr.slice(0,8);                             
+                 console.log(number)
+             }
+             // User must select at least 1 character option or the generate function will be called again
+             if (!lowerCaseEl && !upperCaseEl && !numberEl && !symbolEl) {
+                 alert("You must select at least one option for password characters.")
+                 generatePassword();          
+         };
+             resultEl = newPassword(length, masterPassword);
+}
+ 
+//Password Generation Function
+newPassword = function (length, masterPassword) {
 
+    //Iniitalize final password as blank string
+    let userPassword = '';
 
+    //Append random character from the passwordOptions
+    for (let i = 0; i < length; i++) {
+        userPassword += masterPassword.charAt(Math.floor(Math.random() * masterPassword.length))
+    };
+    resultEl.innerHTML = userPassword;
+}
 
+function copyClip() {
+    // Get the text field
+    var copyBtn = document.getElementById("password");
+  
 
-// password min length 8 and max of 128
-// 1. how to get info from user
-// 2. do something with info
-        // a. check if user wants upper case
-        // b. check if user want lower case
-        // c. check if user wants special character
-        // d. check if user wants a number
-            // if yes than 
-                // console the input
-// 2. 
-        
+    //Select the text field
+    copyBtn.select();
+    copyBtn.setSelectionRange(0, 99999); 
+  
+    //Copy the text inside the text field
+    document.execCommand("copy");
+  
+    //Alert copied/failed message
+    if (copyBtn.value === '') {
+            setMessage('You must generate a password first!', 'red')
+        } else 
+            setMessage('Copied to the clipboard', 'green')
+        };
+      
+ 
+// Will flash a success alert if copied to clipboard or failed alert  and will time out after 3 seconds
+function setMessage(msg, color) {
+        message.style.color = color
+        message.textContent = msg;
+  setTimeout(function() {
+    document.getElementById('message').remove();
+  }, 3000)
+}
 
 
 
